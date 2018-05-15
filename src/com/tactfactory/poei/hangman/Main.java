aa57d33b9699d6df53c4b120f1b803a09845e8f8
Jonathan Poncy <jonathan.poncy@tactfactory.com>
2018-05-15 17:39:14 +0200

    hang - 1.2

--- src/com/tactfactory/poei/hangman/Main.java
+++ src/com/tactfactory/poei/hangman/Main.java
@@ -1,15 +1,20 @@
 package com.tactfactory.poei.hangman;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        String mistery = "mystere";
+        String[] words = new String[] {"mistere", "introuvable", "securise", "fort", "toto", "viagra", "amidon"};
         Scanner scan = new Scanner(System.in);
         String line;
 
+        // Select the mystery word.
+        String mystery = words[new Random().nextInt(words.length)];
+
         System.out.println("Essai de trouver le mot mistère, il est introuvable : ");
+        System.out.println("Indice : " + mystery);
 
         /*
         do {
@@ -19,6 +24,7 @@ public class Main {
         */
         while ("".equals((line = scan.nextLine().trim())));
 
-        System.out.println(line + (mistery.equals(line) ? " => bravo" : " n'est pas le mot \"mystère\""));
+        System.out.println(line + (mystery.equals(line) ? " => bravo" : " n'est pas le mot \"mystère\", "
+                + "il fallait trouver \"" + mystery + "\""));
     }
 }
