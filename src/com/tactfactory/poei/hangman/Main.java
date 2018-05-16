d1751b02834d55093203d30c240485f7837acf26
Jonathan Poncy <jonathan.poncy@tactfactory.com>
2018-05-16 12:21:34 +0200

    hang - 3.2

--- src/com/tactfactory/poei/hangman/Main.java
+++ src/com/tactfactory/poei/hangman/Main.java
@@ -1,5 +1,6 @@
 package com.tactfactory.poei.hangman;
 
+import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -10,34 +11,79 @@ public class Main {
         Scanner scan = new Scanner(System.in);
         String line;
         int life = 5;
-        boolean found;
+        boolean found = false;
+        char[] state;
 
         // Select the mystery word.
         String mystery = words[new Random().nextInt(words.length)];
 
+        // Initialize array state --- word with same size than mystery but with _ instead of letters.
+        Arrays.fill(state = new char[mystery.length()], '_');
+
+        // Initialize state --- word with same size than mystery but with _ instead of letters.
+        /*
+         * To create state as string.
+         * for (int i = 0; i < mystery.length(); ++ i) state += "_"; // Need initialize
+         *
+         * // OR.
+         *
+         * state = Arrays.asList(mystery.split("")).stream().map(e -> "_").collect(Collectors.joining(""));
+         *
+         * // OR.
+         *
+         * state = IntStream.range(0, mystery.length()).mapToObj(i -> "_").collect(Collectors.joining(""));
+         *
+         * // OR.
+         *
+         * StringBuilder sBuilder = new StringBuilder(mystery.length());
+         * for (int i = 0; i < mystery.length(); ++ i) sBuilder.append('_');
+         * state = sBuilder.toString();
+         */
+
         System.out.println("Essai de trouver le mot mistère, il est introuvable.");
         System.out.println("Indice : " + mystery);
 
-        /*
         do {
-            line = scan.nextLine().trim();
-        } while ("".equals(line));
-        // OR
-        */
-        do {
+            System.out.println("");
+            System.out.println(String.valueOf(state));
             // Print "question".
             System.out.println("Il vous reste " + life + " vie" + (life == 1 ? "" : "s") + " : ");
 
             // Wait correct try.
             while ("".equals((line = scan.nextLine().trim())));
 
-            if (!(found = line.equals(mystery))) {
-                -- life;
+            if (line.length() == 1) { // If try letter.
+                char inputLetter = line.charAt(0); // The only one letter.
+                boolean letterFound = false;
+
+                for (byte i = 0; i < mystery.length(); ++ i) {
+                    char letter = mystery.charAt(i);
+
+                    if (letter == inputLetter) {
+                        state[i] = letter;
+                        letterFound = true;
+                    }
+                }
+
+                if (letterFound /* possibility to to following code instead of body : && !String.valueOf(state).contains("_")*/) {
+                    for (int i = 0; i < state.length; ++ i) {
+                        if (state[i] == '_') break;
+                        else if (i == state.length - 1) {
+                            found = true;
+                        }
+                    }
+                } else -- life;
+            } else { // Try word.
+                if (!(found = line.equals(mystery))) {
+                    -- life;
+                }
             }
         } while (!found && life > 0);
 
         scan.close();
 
+        System.out.println(String.valueOf(state));
+        System.out.println("");
         System.out.println(found ? "Bravo !" : "... Le mot correct était \"" + mystery + "\" !");
     }
 }
