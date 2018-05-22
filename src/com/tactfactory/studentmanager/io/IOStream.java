e9bf3d8e27b271609178d069e67755b42c08060c
Jonathan Poncy <jonathan.poncy@tactfactory.com>
2018-05-22 16:37:40 +0200

    Ajout interface pour Q3

---
+++ src/com/tactfactory/studentmanager/io/IOStream.java
@@ -0,0 +1,12 @@
+
+package com.tactfactory.studentmanager.io;
+
+/** Represents our input/output systems (console, file, RAM). */
+public interface IOStream {
+
+    /** Print the message into the implemented output. */
+    void write(String msg);
+
+    /** Read a string from input stream. */
+    String readString();
+}
